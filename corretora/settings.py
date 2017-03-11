1b8d32b49c95ddf73ae1a0d00352f745e6de3575
fernando <fjanuarioazevedo@gmail.com>
2017-03-11 08:57:32 -0300

    a

--- corretora/settings.py
+++ corretora/settings.py
@@ -153,7 +153,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'fjanuarioazevedo@gmail.com'
 EMAIL_HOST_PASSWORD = 'f5u5v5p5'
-# EMAIL_PORT = 587
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'fjanuarioazevedo@gmail.com'
 
